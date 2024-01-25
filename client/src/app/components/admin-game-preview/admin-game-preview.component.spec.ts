30824ed876e3a444f8a273ee1793c402171bfb37
gaetan <github@gagolino.com>
2024-01-25 16:24:59 -0500

    add test

--- client/src/app/components/admin-game-preview/admin-game-preview.component.spec.ts
+++ client/src/app/components/admin-game-preview/admin-game-preview.component.spec.ts
@@ -1,19 +1,64 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-// import { AdminGamePreviewComponent } from './admin-game-preview.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { By } from '@angular/platform-browser';
+import { AdminGameDetails } from '@app/classes/game-details';
+import { AdminGamePreviewComponent } from './admin-game-preview.component';
 
 describe('AdminGamePreviewComponent', () => {
-    // let component: AdminGamePreviewComponent;
-    // let fixture: ComponentFixture<AdminGamePreviewComponent>;
-    // beforeEach(() => {
-    //   TestBed.configureTestingModule({
-    //     declarations: [AdminGamePreviewComponent]
-    //   });
-    //   fixture = TestBed.createComponent(AdminGamePreviewComponent);
-    //   component = fixture.componentInstance;
-    //   fixture.detectChanges();
-    // });
-    // it('should create', () => {
-    //   expect(component).toBeTruthy();
-    // });
+    let component: AdminGamePreviewComponent;
+    let fixture: ComponentFixture<AdminGamePreviewComponent>;
+    let mockGameDetails: AdminGameDetails;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [MatIconModule, AdminGamePreviewComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(async () => {
+        fixture = TestBed.createComponent(AdminGamePreviewComponent);
+        component = fixture.componentInstance;
+        mockGameDetails = {
+            name: 'Test Game',
+            id: 'test-game',
+            description: 'This is a test game',
+            image: '#',
+            lastModified: '01-01-2024',
+            isVisible: true,
+        };
+        component.gameDetails = mockGameDetails;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should emit edit event when export button is clicked', () => {
+        spyOn(component.edit, 'emit');
+        const exportButton = fixture.debugElement.query(By.css('.admin-edit')).nativeElement;
+        exportButton.click();
+        expect(component.edit.emit).toHaveBeenCalled();
+    });
+
+    it('should emit delete event when export button is clicked', () => {
+        spyOn(component.delete, 'emit');
+        const exportButton = fixture.debugElement.query(By.css('.admin-delete')).nativeElement;
+        exportButton.click();
+        expect(component.delete.emit).toHaveBeenCalled();
+    });
+
+    it('should emit export event when export button is clicked', () => {
+        spyOn(component.export, 'emit');
+        const exportButton = fixture.debugElement.query(By.css('.admin-download')).nativeElement;
+        exportButton.click();
+        expect(component.export.emit).toHaveBeenCalled();
+    });
+
+    it('should emit toggleVisibility event when export button is clicked', () => {
+        spyOn(component.toggleVisibility, 'emit');
+        const exportButton = fixture.debugElement.query(By.css('.admin-visibility')).nativeElement;
+        exportButton.click();
+        expect(component.toggleVisibility.emit).toHaveBeenCalled();
+    });
 });
