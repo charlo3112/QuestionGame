03b1c8a4703f11793d1dca2905e830a63934a332
gaetan <github@gagolino.com>
2024-01-25 15:52:00 -0500

    update login admin

--- client/src/app/components/admin-login/admin-login.component.ts
+++ client/src/app/components/admin-login/admin-login.component.ts
@@ -1,13 +1,20 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector: 'app-admin-login',
     templateUrl: './admin-login.component.html',
     styleUrls: ['./admin-login.component.scss'],
+    standalone: true,
+    imports: [MatButtonModule, MatInputModule, MatIconModule, FormsModule, ReactiveFormsModule, NgIf],
 })
 export class AdminLoginComponent {
     @Output() loginSuccess = new EventEmitter<boolean>();
+    error = false;
 
     loginForm = new FormGroup({
         password: new FormControl('', [Validators.required]),
@@ -15,8 +22,11 @@ export class AdminLoginComponent {
 
     onSubmit() {
         if (this.loginForm.valid) {
-            if (this.loginForm.value.password === 'admin') {
+            if (this.loginForm.value.password === 'log2990-202') {
                 this.loginSuccess.emit(true);
+            } else {
+                this.error = true;
+                this.loginForm.reset();
             }
         }
     }
