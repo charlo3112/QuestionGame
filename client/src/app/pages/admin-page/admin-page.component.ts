75f5b7ddd2eafe8c19e9d626f7e12117993aa74e
gaetan <github@gagolino.com>
2024-01-25 16:42:42 -0500

    add test admin page

--- client/src/app/pages/admin-page/admin-page.component.ts
+++ client/src/app/pages/admin-page/admin-page.component.ts
@@ -1,47 +1,55 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { AdminGameDetails } from '@app/classes/game-details';
+import { AdminGamePreviewComponent } from '@app/components/admin-game-preview/admin-game-preview.component';
+import { AdminLoginComponent } from '@app/components/admin-login/admin-login.component';
 
 @Component({
     selector: 'app-admin-page',
     templateUrl: './admin-page.component.html',
     styleUrls: ['./admin-page.component.scss'],
+    standalone: true,
+    imports: [NgIf, NgFor, AdminGamePreviewComponent, AdminLoginComponent],
 })
 export class AdminPageComponent {
     login: boolean = false;
 
     games: AdminGameDetails[] = [
-        new AdminGameDetails('id', 'Game 1rjntjrjtrjtjrtrtrjtjrtnjrjtn', 'url-to-image', 'description', true, '2021-03-03'),
-        new AdminGameDetails('id2', 'Game 2', 'url-to-image', 'description', true, '2021-03-03'),
-        new AdminGameDetails('id3', 'Game 3', 'url-to-image', 'description', true, '2021-03-03'),
-        new AdminGameDetails('id4', 'Game 4', 'url-to-image', 'description', true, '2021-03-03'),
-        new AdminGameDetails('id5', 'Game 5', 'url-to-image', 'description', true, '2021-03-03'),
-        new AdminGameDetails('id6', 'Game 6', 'url-to-image', 'description', true, '2021-03-03'),
-        new AdminGameDetails('id7', 'Game 7', 'url-to-image', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id', 'Game 1rjntjrjtrjtjrtrtrjtjrtnjrjtn', '#', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id2', 'Game 2', '#', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id3', 'Game 3', '#', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id4', 'Game 4', '#', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id5', 'Game 5', '#', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id6', 'Game 6', '#', 'description', true, '2021-03-03'),
+        new AdminGameDetails('id7', 'Game 7', '#', 'description', true, '2021-03-03'),
     ];
 
     editGame(id: string) {
         alert(`Edit game with id ${id}`);
+
+        // TODO: Add server call to edit game
     }
 
     deleteGame(id: string) {
-        // Logic to delete game by id
         alert(`Delete game with id ${id}`);
+
+        // TODO: Add server call to delete game
     }
 
     exportGame(id: string) {
-        // Logic to export game by id
         alert(`Export game with id ${id}`);
+
+        // TODO: Add server call to export game
     }
 
     toggleGameVisibility(id: string) {
-        // Logic to toggle visibility of the game by id
-        // This could be an update to the game's details and a subsequent refresh of the list
-        // alert(`Toggle visibility of game with id ${id}`);
         const game = this.games.find((g) => g.id === id);
         if (!game) {
             return;
         }
         game.isVisible = !game.isVisible;
+
+        // TODO: Add server call to update game visibility
     }
 
     handleLogin(success: boolean) {
