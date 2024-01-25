13ef73a953709c4c169c5c7ebebe22b076f5bc96
marcantoinem <marc-antoine.m@outlook.com>
2024-01-25 15:50:12 -0500

    Add input

--- client/src/app/components/question/question.component.spec.ts
+++ client/src/app/components/question/question.component.spec.ts
@@ -1,20 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { QuestionComponent } from '@app/components/question/question.component';
+import { QUESTION_PLACEHOLDER } from '@app/interfaces/question';
 
 describe('QuestionComponent', () => {
     let component: QuestionComponent;
     let fixture: ComponentFixture<QuestionComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [QuestionComponent],
-            providers: [],
-        }).compileComponents();
-    });
-
     beforeEach(() => {
         fixture = TestBed.createComponent(QuestionComponent);
         component = fixture.componentInstance;
+        component.question = QUESTION_PLACEHOLDER;
         fixture.detectChanges();
     });
 
