8c74037b62ad94b65b6c5dc3fa351c75a9a6118d
GuilhemDM <82975763+GuilhemDM@users.noreply.github.com>
2024-01-31 10:53:37 -0500

    Mise en page et drag and drop

--- client/src/app/components/create-question/create-question.component.ts
+++ client/src/app/components/create-question/create-question.component.ts
@@ -2,48 +2,67 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSliderModule } from '@angular/material/slider';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-create-question',
     templateUrl: './create-question.component.html',
     styleUrls: ['./create-question.component.scss'],
-    imports: [
-        CommonModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatSliderModule,
-        MatDividerModule,
-        MatListModule,
-        MatButtonModule,
-        MatIconModule,
-        FormsModule,
-        MatSlideToggleModule,
-    ],
+    imports: [CommonModule, MatFormFieldModule, MatInputModule, MatSliderModule, MatButtonModule, MatIconModule, FormsModule, MatCheckboxModule],
     standalone: true,
 })
 export class CreateQuestionComponent {
-    choices: { id: number; value: string }[] = [];
+    constructor(private router: Router) {}
+    choices: { id: number; value: string; answer: boolean }[] = [];
     choiceInput: string = '';
+    hasAnswer(): boolean {
+        let hasChecked = false;
+        let hasUnchecked = false;
+
+        for (const choice of this.choices) {
+            if (choice.answer) {
+                hasChecked = true;
+            } else {
+                hasUnchecked = true;
+            }
+
+            if (hasChecked && hasUnchecked) {
+                break;
+            }
+        }
+        return hasChecked && hasUnchecked;
+    }
 
     addChoice() {
-        const newChoice = {
-            id: this.choices.length + 1,
-            value: this.choiceInput,
-        };
-        this.choices.push(newChoice);
-        this.choiceInput = '';
+        if (this.choices.length < 4) {
+            const newChoice = {
+                id: this.choices.length + 1,
+                value: this.choiceInput,
+                answer: false,
+            };
+            this.choices.push(newChoice);
+            this.choiceInput = '';
+        } else {
+            window.alert('Vous ne pouvez pas ajouter plus de 4 choix.');
+        }
     }
 
     deleteChoice(index: number): void {
         this.choices.splice(index, 1);
+    }
+
+    save() {
+        if (this.choices.length < 2) {
+            window.alert("Veuillez ajouter au moins deux choix de réponse avant d'enregistrer la question");
+        } else if (!this.hasAnswer()) {
+            window.alert("Il faut au moins une réponse et un choix éronné avant d'enregistrer la question");
+        } else {
+            this.router.navigate(['/new']);
+        }
     }
 }
--- client/src/app/pages/create-page/create-page.component.ts
+++ client/src/app/pages/create-page/create-page.component.ts
@@ -1,7 +1,7 @@
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -21,16 +21,50 @@ import { RouterModule } from '@angular/router';
         MatInputModule,
         MatSelectModule,
         MatSliderModule,
-        MatDividerModule,
         MatListModule,
         MatButtonModule,
         MatIconModule,
         RouterModule,
+        DragDropModule,
     ],
 })
 export class CreatePageComponent {
-    questions: string[] = ["Pourquoi aller à l'école?", "Qu'est-ce que la vie?"];
+    questions: string[] = [
+        'Peut-on vraiment tout savoir ?',
+        'Quel est le sens d’une bonne vie ?',
+        'Y a-t-il un Dieu ?',
+        'Qu’est-ce que la conscience ?',
+        'Y a-t-il un ordre inhérent dans la nature ?',
+        'Est-il plus important d’être respecté ou aimé ?',
+        'Sommes-nous dans la Matrice ?',
+        'Sommes-nous devenus moins heureux à l’ère de la technologie ?',
+        'Qu’est-ce que les mathématiques ?',
+        'Les humains sont-ils obligés de s’améliorer ?',
+        'Y a-t-il un sens à la vie ?',
+        'Avoir un gros ego est-il un trait négatif ou positif ?',
+        'Existe-t-il une mortalité absolue ?',
+        'Le but le plus important dans la vie est-il de trouver le bonheur ?',
+        'Avons-nous le libre arbitre ?',
+        'La vie a-t-elle besoin d’un but et d’un but ?',
+        'Tueriez-vous 10 personnes pour en sauver 100 ?',
+        'Qu’est-ce que le bonheur ?',
+        'Comment les gens peuvent-​ils croire aux vérités sans preuves ?',
+        'Est-il plus facile d’aimer ou d’être aimé ?',
+        'Quelle est l’heure ?',
+        'Les actes de bonté ont-ils un motif ?',
+        'L’esprit ou la matière est-il plus réel ?',
+        'L’amour est-il simplement un désir physique ou quelque chose de plus ?',
+        'D’où viennent les pensées ?',
+        'Le mal vient-il de l’intérieur, et si oui pourquoi ?',
+        'Qu’est-ce que la beauté ?',
+        'Où étaient les gens avant leur naissance ?',
+        'Qu’est-ce que la véritable amitié ?',
+        'Comment fonctionne la gravité ?',
+    ];
     deleteQuestion(index: number): void {
         this.questions.splice(index, 1);
+    }
+    drop(event: CdkDragDrop<string[]>): void {
+        moveItemInArray(this.questions, event.previousIndex, event.currentIndex);
     }
 }
