4e20316a58dcfa29652d6971b524dd2361c800d1
Charles-Olivier Caron <charlo3112@gmail.com>
2024-01-30 22:32:53 -0500

    new tests created pass

--- client/src/app/components/play-area/play-area.component.spec.ts
+++ client/src/app/components/play-area/play-area.component.spec.ts
@@ -43,7 +43,10 @@ describe('PlayAreaComponent', () => {
     });
 
     it('ngAfterViewInit should call startTimer with correct time', fakeAsync(() => {
+<<<<<<< HEAD
         // const choices: Choice[] = [];
+=======
+>>>>>>> 85e3ff3 (new tests created pass)
         component.ngAfterViewInit();
         tick();
         expect(timeServiceSpy.startTimer).toHaveBeenCalledWith(component['timer']);
@@ -67,6 +70,7 @@ describe('PlayAreaComponent', () => {
 
     it('time should return timeService.time', () => {
         expect(component.time).toEqual(timeServiceSpy.time);
+<<<<<<< HEAD
     });
 
     it('should populate choices when question input is provided', () => {
@@ -88,5 +92,7 @@ describe('PlayAreaComponent', () => {
         component.ngOnInit();
         expect(component.populateChoices).not.toHaveBeenCalled();
         expect(component.choices.length).toEqual(0);
+=======
+>>>>>>> 85e3ff3 (new tests created pass)
     });
 });
