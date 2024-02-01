1cc0e70c16d6f889cf7fad428d3f82fd9e47f4dc
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-02-01 14:16:35 -0500

    modifications apportées à app.module pour gérer la base de données

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,14 +1,30 @@
 import { GameController } from '@app/controllers/game/game.controller';
 import { QuestionController } from '@app/controllers/question/question.controller';
-import { ChatGateway } from '@app/gateways/chat/chat.gateway';
+// import { ChatGateway } from '@app/gateways/chat/chat.gateway';
+import { Game, gameSchema } from '@app/model/database/game';
 import { GameService } from '@app/services/game/game.service';
 import { QuestionService } from '@app/services/question/question.service';
 import { Logger, Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Question, questionSchema } from './model/database/question';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
+            }),
+        }),
+        MongooseModule.forFeature([
+            { name: Game.name, schema: gameSchema },
+            { name: Question.name, schema: questionSchema },
+        ]),
+    ],
     controllers: [GameController, QuestionController],
-    providers: [ChatGateway, GameService, QuestionService, Logger],
+    providers: [GameService, QuestionService, Logger],
 })
 export class AppModule {}
