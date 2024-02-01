2d37c9b9d2af14d26a020f8bf912462dace68b51
marcantoinem <marc-antoine.m@outlook.com>
2024-02-01 15:53:19 -0500

    Revert "enums folder created"
    This reverts commit 9863394777533f8342a7c515805c7fb3e47cba38.

--- client/src/app/components/play-area/play-area.component.ts
+++ client/src/app/components/play-area/play-area.component.ts
@@ -43,7 +43,6 @@ export class PlayAreaComponent implements AfterViewInit, OnInit {
         this.buttonPressed = event.key;
     }
 
-<<<<<<< HEAD
     ngOnInit(): void {
         if (this.question) {
             this.populateChoices();
@@ -69,10 +68,6 @@ export class PlayAreaComponent implements AfterViewInit, OnInit {
 
     chatConfirm() {
         window.alert('Bienvenu au chat');
-=======
-    ngAfterViewInit(): void {
-        this.timeService.startTimer(this.timer);
->>>>>>> 209120a (enums folder created)
     }
 
     // TODO : déplacer ceci dans un service de gestion de la souris!
