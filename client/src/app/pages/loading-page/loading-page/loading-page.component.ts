bead8cf11aa98b5206b28a0259ce4c504d7b6cd6
nicomayr <nicolas.mayr@polymtl.ca>
2024-02-01 14:24:43 -0500

    Added loading page component + test

---
+++ client/src/app/pages/loading-page/loading-page/loading-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-loading-page',
+    templateUrl: './loading-page.component.html',
+    styleUrls: ['./loading-page.component.scss'],
+    standalone: true,
+})
+export class LoadingPageComponent {}
