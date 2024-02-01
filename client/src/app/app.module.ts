a778915a025f1bf93f342456db1ae139d8227fd2
marcantoinem <marc-antoine.m@outlook.com>
2024-02-01 13:19:23 -0500

    Remove material page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
-import { AppMaterialModule } from '@app/modules/material.module';
 import { AdminPageComponent } from '@app/pages/admin-page/admin-page.component';
 import { AppComponent } from '@app/pages/app/app.component';
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { QuestionComponent } from './components/question/question.component';
 
 import { MatButtonModule } from '@angular/material/button';
@@ -21,9 +19,8 @@ import { MatInputModule } from '@angular/material/input';
  * Otherwise Angular Cli will not know in which module to put new component
  */
 @NgModule({
-    declarations: [AppComponent, MaterialPageComponent],
+    declarations: [AppComponent],
     imports: [
-        AppMaterialModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         BrowserModule,
