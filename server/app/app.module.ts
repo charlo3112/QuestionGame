38f1ef2794fa19d9c4611310a8642fe5dd310a83
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-02-01 13:36:03 -0500

    début de recherche pour régler le problème au lancement du serveur

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,11 +1,10 @@
-import { Logger, Module } from '@nestjs/common';
-import { ConfigModule /* , ConfigService*/ } from '@nestjs/config';
-// import { Course, courseSchema } from '@app/model/database/course';
 import { GameController } from '@app/controllers/game/game.controller';
 import { QuestionController } from '@app/controllers/question/question.controller';
 import { ChatGateway } from '@app/gateways/chat/chat.gateway';
 import { GameService } from '@app/services/game/game.service';
 import { QuestionService } from '@app/services/question/question.service';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true })],
