09cb29b54eb6cfc64d59900588e0da0476e3f6c0
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-02-03 10:53:58 -0500

    copie-colle des fichers de services, controller et constants

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -6,10 +6,14 @@ import { ExampleController } from '@app/controllers/example/example.controller';
 import { ChatGateway } from '@app/gateways/chat/chat.gateway';
 import { DateService } from '@app/services/date/date.service';
 import { ExampleService } from '@app/services/example/example.service';
+import { GameController } from './controllers/game.controller/game.controller.controller';
+import { QuestionController } from './controllers/question.controller/question.controller.controller';
+import { GameService } from './services/game/game.service';
+import { QuestionService } from './services/question/question.service';
 
 @Module({
     imports: [ConfigModule.forRoot({ isGlobal: true })],
-    controllers: [DateController, ExampleController],
-    providers: [ChatGateway, DateService, ExampleService, Logger],
+    controllers: [DateController, ExampleController, QuestionController, GameController],
+    providers: [ChatGateway, DateService, ExampleService, Logger, GameService, QuestionService],
 })
 export class AppModule {}
---
+++ server/app/model/database/game.ts
@@ -0,0 +1,61 @@
+import { CreateGameDto } from '@app/model/dto/game/create-game.dto';
+import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+import { Question } from './question';
+
+export type GameDocument = Game & Document;
+
+@Schema()
+export class Game {
+    #id: string;
+    #title: string;
+    #description: string;
+    #duration: number;
+    #lastModification: string;
+    #questions: Question[];
+    #visibility: boolean = true;
+
+    constructor(gameData: CreateGameDto) {
+        this.#id = uuidv4();
+        this.#title = gameData.title;
+        this.#description = gameData.description;
+        this.#duration = gameData.duration;
+        this.#lastModification = new Date().toISOString();
+        this.#questions = gameData.questions;
+    }
+
+    addQuestion(newQuestion: Question) {
+        this.#questions.push(newQuestion);
+    }
+
+    getId(): string {
+        return this.#id;
+    }
+
+    getTitle(): string {
+        return this.#title;
+    }
+
+    getDescription(): string {
+        return this.#description;
+    }
+
+    getDuration(): number {
+        return this.#duration;
+    }
+
+    getLastModification(): string {
+        return this.#lastModification;
+    }
+
+    getQuestions(): Question[] {
+        return this.#questions;
+    }
+
+    getVisibility(): boolean {
+        return this.#visibility;
+    }
+}
+
+export const gameSchema = SchemaFactory.createForClass(Game);
