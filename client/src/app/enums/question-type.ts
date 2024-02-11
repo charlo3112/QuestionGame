9bcbdc4da3c72640ab6a774e44ace7fbe2c2e495
marcantoinem <marc-antoine.m@outlook.com>
2024-02-11 00:18:23 -0500

    Move enum to appropriate folder

---
+++ client/src/app/enums/question-type.ts
@@ -0,0 +1,3 @@
+export enum QuestionType {
+    Qcm = 'qcm',
+}
--- client/src/app/interfaces/question.ts
+++ client/src/app/interfaces/question.ts
@@ -1,8 +1,5 @@
+import { QuestionType } from '@app/enums/question-type';
 import { Choice } from './choice';
-
-export enum QuestionType {
-    Qcm = 'qcm',
-}
 
 export interface Question {
     type: QuestionType;
@@ -10,10 +7,3 @@ export interface Question {
     points: number;
     choices: Choice[];
 }
-
-export const QUESTION_PLACEHOLDER: Question = {
-    type: QuestionType.Qcm,
-    text: 'What is the text to print?',
-    points: 42,
-    choices: [{ text: 'hello_world' }, { text: 'test' }, { text: 'lorem_ipsum' }],
-};
