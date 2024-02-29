adb3f7e9f1c830c1a8983f7fb69e5dfe720ac365
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-02-29 09:59:47 -0500

    retrait de code dupliqué

--- client/src/app/components/score-board/score-board.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-score-board',
-    templateUrl: './score-board.component.html',
-    styleUrls: ['./score-board.component.scss'],
-    standalone: true,
-})
-export class ScoreBoardComponent {}
--- client/src/app/pages/result-page/result-page.component.ts
+++ client/src/app/pages/result-page/result-page.component.ts
@@ -3,13 +3,15 @@ import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ChatComponent } from '@app/components/chat/chat.component';
 import { HistogramComponent } from '@app/components/histogram/histogram.component';
-import { ScoreBoardComponent } from '@app/components/score-board/score-board.component';
+import { LeaderboardComponent } from '@app/components/leaderboard/leaderboard.component';
 
 @Component({
     selector: 'app-result-page',
     templateUrl: './result-page.component.html',
     styleUrls: ['./result-page.component.scss'],
     standalone: true,
-    imports: [MatToolbarModule, ChatComponent, MatCardModule, HistogramComponent, ScoreBoardComponent],
+    imports: [MatToolbarModule, ChatComponent, MatCardModule, HistogramComponent, LeaderboardComponent],
 })
-export class ResultPageComponent {}
+export class ResultPageComponent {
+    showStats: boolean;
+}
