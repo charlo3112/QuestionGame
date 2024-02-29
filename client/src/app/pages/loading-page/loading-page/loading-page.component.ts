52ec2b9cf92e3a000b020054b656b460b9c56917
nicomayr <nicolas.mayr@polymtl.ca>
2024-02-29 14:19:49 -0500

    Loading page degeulasse

--- client/src/app/pages/loading-page/loading-page/loading-page.component.ts
+++ client/src/app/pages/loading-page/loading-page/loading-page.component.ts
@@ -1,9 +1,38 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { RouterModule } from '@angular/router';
+import { ChatComponent } from '@app/components/chat/chat.component';
 
 @Component({
     selector: 'app-loading-page',
     templateUrl: './loading-page.component.html',
     styleUrls: ['./loading-page.component.scss'],
     standalone: true,
+    imports: [NgFor, NgIf, MatIconModule, MatButtonModule, MatTooltipModule, ChatComponent, MatToolbarModule, RouterModule, MatListModule],
 })
-export class LoadingPageComponent {}
+export class LoadingPageComponent {
+    playersPlaceHolder = ['Albert', 'George', 'John', 'Paul', 'Ringo'];
+    roomLocked = false;
+    readonly roomCode = 1234;
+    isHost = true; // TODO: For host only functionalities
+
+    onToggleLock() {
+        this.roomLocked = !this.roomLocked;
+        // TODO: Add logic to actually lock room!
+    }
+
+    onStartGame() {
+        window.alert('Game started!');
+        // TODO: Add logic to actually start game with right players
+    }
+
+    onKickPlayer(player: string) {
+        this.playersPlaceHolder = this.playersPlaceHolder.filter((p) => p !== player);
+        // TODO: Add logic to actually kick player and ban his name
+    }
+}
