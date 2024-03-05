cd9993feeb4d1f65f62b05dbb2b3319c35f7157c
GuilhemDM <82975763+GuilhemDM@users.noreply.github.com>
2024-03-05 15:51:44 -0500

    ajout input histogramme

--- client/src/app/components/histogram/histogram.component.ts
+++ client/src/app/components/histogram/histogram.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { Choice, ChoiceWithCounter } from '@app/classes/choice';
-import { QuestionType } from '@common/constants';
 import { Question } from '@app/interfaces/question';
 
 @Component({
@@ -15,34 +14,7 @@ import { Question } from '@app/interfaces/question';
     standalone: true,
 })
 export class HistogramComponent {
-    choix1question1 = new ChoiceWithCounter('Guillaume en boit', true, 5);
-    choix2question1 = new ChoiceWithCounter('Guillaume en a apporté 2 boites', false, 3);
-    choix3question1 = new ChoiceWithCounter("C'est du lichi", false, 2);
-    question1: Question = {
-        type: QuestionType.QCM,
-        text: "Pourquoi le jus de lichi n'est pas bon?",
-        points: 69,
-        choices: [this.choix1question1, this.choix2question1, this.choix3question1],
-    }
-    choix1question2 = new ChoiceWithCounter('Les temps de compilation sont abominables', false, 1);
-    choix2question2 = new ChoiceWithCounter("C'est quoi cette question?", true, 4);
-    choix3question2 = new ChoiceWithCounter('Le javascript est une erreur.', true, 156);
-    choix4question2 = new ChoiceWithCounter('Les deux sont horribles', false, 2);
-    question2: Question = {
-        type: QuestionType.QCM,
-        text: 'Pourquoi le Rust est un langage supérieur pour le frontend?',
-        points: 42,
-        choices: [this.choix1question2, this.choix2question2, this.choix3question2, this.choix4question2],
-    }
-    choix1question3 = new ChoiceWithCounter('Premier choix', true, 1);
-    choix2question3 = new ChoiceWithCounter('Deuxieme choix', false, 8);
-    question3: Question = {
-        type: QuestionType.QCM,
-        text: 'Question 3',
-        points: 42,
-        choices: [this.choix1question3, this.choix2question3],
-    }
-    listQuestions: Question[] = [this.question1, this.question2, this.question3];
+    @Input() listQuestions: Question[];
     questionDisplayed: number = 0;
 
     isChoiceWithCounter(choice: Choice): choice is ChoiceWithCounter {
@@ -51,27 +23,25 @@ export class HistogramComponent {
     previousQuestion() {
         if (this.questionDisplayed !== 0) {
             this.questionDisplayed--;
-        }
-        else {
+        } else {
             this.questionDisplayed = this.listQuestions.length - 1;
         }
     }
     nextQuestion() {
         if (this.questionDisplayed !== this.listQuestions.length - 1) {
             this.questionDisplayed++;
-        }
-        else {
+        } else {
             this.questionDisplayed = 0;
         }
     }
-    getMaxCounter(): number{
+    getMaxCounter(): number {
         const question = this.listQuestions[this.questionDisplayed];
         let counters: number[] = [];
         for (let choice of question.choices) {
             if (this.isChoiceWithCounter(choice)) {
                 counters.push(choice.counter);
             }
-        }    
+        }
         if (counters.length > 0) {
             return Math.max(...counters);
         } else {
