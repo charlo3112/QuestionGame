3628eeb85588db7709245408b45f4f797a826a4e
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-03-04 20:34:40 -0500

    correction à faire pour leaderboard

--- client/src/app/pages/admin-game-page/admin-game-page.component.ts
+++ client/src/app/pages/admin-game-page/admin-game-page.component.ts
@@ -5,20 +5,35 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { AnswersComponent } from '@app/components/answers/answers.component';
 import { ChatComponent } from '@app/components/chat/chat.component';
 import { LeaderboardComponent } from '@app/components/leaderboard/leaderboard.component';
-import { Question } from '@app/interfaces/question';
+import { QUESTION_PLACEHOLDER, Question } from '@app/interfaces/question';
 import { GameService } from '@app/services/game.service';
 
 @Component({
     selector: 'app-admin-game-page',
     templateUrl: './admin-game-page.component.html',
     styleUrls: ['./admin-game-page.component.scss'],
-    imports: [ChatComponent, LeaderboardComponent, CommonModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterModule, MatToolbarModule],
+    imports: [
+        ChatComponent,
+        LeaderboardComponent,
+        CommonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        RouterModule,
+        MatToolbarModule,
+        AnswersComponent,
+    ],
     standalone: true,
 })
 export class AdminGamePageComponent {
     @Input() question: Question;
 
     constructor(readonly gameService: GameService) {}
+
+    fetchQuestion() {
+        this.question = QUESTION_PLACEHOLDER;
+    }
 }
