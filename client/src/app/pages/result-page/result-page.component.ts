8345fc590cfa60f7088925c0058cbb155779e28d
GuilhemDM <82975763+GuilhemDM@users.noreply.github.com>
2024-03-05 15:02:45 -0500

    histogramme

--- client/src/app/pages/result-page/result-page.component.ts
+++ client/src/app/pages/result-page/result-page.component.ts
@@ -6,13 +6,14 @@ import { HistogramComponent } from '@app/components/histogram/histogram.componen
 import { CommonModule } from '@angular/common';
 import { LeaderboardComponent } from '@app/components/leaderboard/leaderboard.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-result-page',
     templateUrl: './result-page.component.html',
     styleUrls: ['./result-page.component.scss'],
     standalone: true,
-    imports: [MatToolbarModule, ChatComponent, MatCardModule, HistogramComponent, CommonModule, LeaderboardComponent, MatTabsModule],
+    imports: [MatToolbarModule, ChatComponent, MatCardModule, HistogramComponent, CommonModule, LeaderboardComponent, MatTabsModule, MatButtonModule],
 })
 export class ResultPageComponent {
     showStats: boolean;
