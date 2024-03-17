1d78908df97fee27f9e956cc3eee11117e32aa1b
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-03-17 13:04:03 -0400

    fixed test in admin game-view

--- client/src/app/components/admin-game-view/admin-game-view.component.spec.ts
+++ client/src/app/components/admin-game-view/admin-game-view.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { GameService } from '@app/services/game.service';
 import { AdminGameViewComponent } from './admin-game-view.component';
 
-describe('AdminGameViewComponentComponent', () => {
+describe('AdminGameViewComponent', () => {
     let component: AdminGameViewComponent;
     let fixture: ComponentFixture<AdminGameViewComponent>;
+    const snackBarMock = {
+        open: jasmine.createSpy('open'),
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [AdminGameViewComponent],
+            imports: [AdminGameViewComponent, BrowserAnimationsModule, NoopAnimationsModule],
+            providers: [GameService, { provide: MatSnackBar, useValue: snackBarMock }],
         });
         fixture = TestBed.createComponent(AdminGameViewComponent);
         component = fixture.componentInstance;
