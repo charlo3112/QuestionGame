89f062db0095838383809b9633306a4c214f261b
gaetan <github@gagolino.com>
2024-03-19 22:25:50 -0400

    fix score

--- client/src/app/services/game/game.service.ts
+++ client/src/app/services/game/game.service.ts
@@ -216,7 +216,7 @@ export class GameService implements OnDestroy {
     }
 
     confirmQuestion() {
-        if (this.state !== GameState.AskingQuestion) {
+        if (this.state === GameState.AskingQuestion) {
             this.websocketService.validateChoice();
             return;
         }
