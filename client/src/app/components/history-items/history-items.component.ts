4c68f2f9f6021c679a1704bfb855ced5266eb492
Charles-Olivier Caron <charlo3112@gmail.com>
2024-03-24 20:10:30 -0400

    sort options added to html

--- client/src/app/components/history-items/history-items.component.ts
+++ client/src/app/components/history-items/history-items.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -7,9 +8,11 @@ import { MatCardModule } from '@angular/material/card';
     templateUrl: './history-items.component.html',
     styleUrls: ['./history-items.component.scss'],
     standalone: true,
-    imports: [MatCardModule, CommonModule],
+    imports: [MatCardModule, CommonModule, FormsModule],
 })
 export class HistoryItemsComponent {
+    selectedSort: string = 'name';
+    selectedSortOrder: string = 'asc';
     items = [
         {
             name: 'This is a very long game name for test purposes',
@@ -30,4 +33,15 @@ export class HistoryItemsComponent {
             bestScore: 78,
         },
     ];
+    sortOptions = [
+        { value: 'name', label: 'Nom du jeu' },
+        { value: 'creationDate', label: 'Date de création' },
+    ];
+
+    sortOrderOptions = [
+        { value: 'asc', label: 'Croissant' },
+        { value: 'desc', label: 'Décroissant' },
+    ];
+
+    sortItems(value: string, order: string) {}
 }
