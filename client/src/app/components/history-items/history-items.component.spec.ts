45c9619c884f0878c8ff205eaa6be1eb7ff1b762
GuilhemDM <82975763+GuilhemDM@users.noreply.github.com>
2024-03-28 14:39:18 -0400

    fixed two tests

--- client/src/app/components/history-items/history-items.component.spec.ts
+++ client/src/app/components/history-items/history-items.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { CommunicationService } from '@app/services/communication/communication.service';
+import { History } from '@common/interfaces/history';
+import { Result } from '@common/interfaces/result';
+import { of } from 'rxjs';
 import { HistoryItemsComponent } from './history-items.component';
 import SpyObj = jasmine.SpyObj;
 
@@ -10,9 +14,16 @@ describe('HistoryItemsComponent', () => {
 
     beforeEach(async () => {
         communicationServiceSpy = jasmine.createSpyObj('CommunicationService', ['deleteHistories', 'getHistories']);
+        const mockHistoryResponse: Result<History[]> = {
+            ok: true,
+            value: [],
+        };
+
+        communicationServiceSpy.getHistories.and.returnValue(of(mockHistoryResponse));
+
         TestBed.configureTestingModule({
             providers: [{ provide: CommunicationService, useValue: communicationServiceSpy }],
-            imports: [HistoryItemsComponent],
+            imports: [HistoryItemsComponent, BrowserAnimationsModule, NoopAnimationsModule],
         });
         fixture = TestBed.createComponent(HistoryItemsComponent);
         component = fixture.componentInstance;
--- client/src/app/pages/history-page/history-page.component.spec.ts
+++ client/src/app/pages/history-page/history-page.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HistoryPageComponent } from './history-page.component';
 
 describe('HistoryPageComponent', () => {
@@ -7,7 +10,7 @@ describe('HistoryPageComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HistoryPageComponent],
+            imports: [HistoryPageComponent, RouterTestingModule, HttpClientTestingModule, BrowserAnimationsModule, NoopAnimationsModule],
         });
         fixture = TestBed.createComponent(HistoryPageComponent);
         component = fixture.componentInstance;
