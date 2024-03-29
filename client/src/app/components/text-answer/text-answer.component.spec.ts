4023152d257f5b4ea8c19d159acf172d3c3251ad
guillaumestpierre <113061567+guillaumestpierre@users.noreply.github.com>
2024-03-29 18:08:32 -0400

    fixed tests after adding QRL

--- client/src/app/components/text-answer/text-answer.component.spec.ts
+++ client/src/app/components/text-answer/text-answer.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { GameService } from '@app/services/game/game.service';
 import { TextAnswerComponent } from './text-answer.component';
 
 describe('TextAnswerComponent', () => {
     let component: TextAnswerComponent;
     let fixture: ComponentFixture<TextAnswerComponent>;
-
+    let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
     beforeEach(() => {
+        snackBarSpy = jasmine.createSpyObj('MatSnackBar', ['open']);
         TestBed.configureTestingModule({
             imports: [TextAnswerComponent],
+            providers: [GameService, { provide: MatSnackBar, useValue: snackBarSpy }],
         });
         fixture = TestBed.createComponent(TextAnswerComponent);
         component = fixture.componentInstance;
