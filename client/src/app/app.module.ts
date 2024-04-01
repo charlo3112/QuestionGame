30ee51a2e9d81a1e17cb70128b5953016cbebf06
Charles-Olivier Caron <charlo3112@gmail.com>
2024-04-01 14:15:11 -0400

    app module imports added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from '@app/modules/app-routing.module';
@@ -9,10 +14,15 @@ import { CommunicationService } from '@app/services/communication/communication.
 import { ValidationService } from '@app/services/validation/validation.service';
 import { WebSocketService } from '@app/services/websocket/websocket.service';
 import { AbandonDialogComponent } from './components/abandon-dialog/abandon-dialog.component';
+import { AnswersComponent } from './components/answers/answers.component';
+import { CreateQuestionComponent } from './components/create-question/create-question.component';
 import { HistogramComponent } from './components/histogram/histogram.component';
 import { HistoryItemsComponent } from './components/history-items/history-items.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { QuestionBankComponent } from './components/question-bank/question-bank.component';
+import { CreatePageComponent } from './pages/create-page/create-page.component';
 import { HistoryPageComponent } from './pages/history-page/history-page.component';
+import { QuestionBankPageComponent } from './pages/question-bank-page/question-bank-page.component';
 import { ResultPageComponent } from './pages/result-page/result-page.component';
 import { GameService } from './services/game/game.service';
 
@@ -31,7 +41,7 @@ import { GameService } from './services/game/game.service';
         AppRoutingModule,
         BrowserAnimationsModule,
         BrowserModule,
-        CreatePageComponent,
+        ResultPageComponent,
         CreateQuestionComponent,
         CreatePageComponent,
         CreateQuestionComponent,
