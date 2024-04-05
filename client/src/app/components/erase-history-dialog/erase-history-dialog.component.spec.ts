d54a30f48d1d07070f3a390d7cf3b0a55d607623
Charles-Olivier Caron <charlo3112@gmail.com>
2024-04-05 00:07:54 -0400

    erase-history-dialog tests added for component

--- client/src/app/components/erase-history-dialog/erase-history-dialog.component.spec.ts
+++ client/src/app/components/erase-history-dialog/erase-history-dialog.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MatDialogRef } from '@angular/material/dialog';
 import { EraseHistoryDialogComponent } from './erase-history-dialog.component';
 
 describe('EraseHistoryDialogComponent', () => {
     let component: EraseHistoryDialogComponent;
     let fixture: ComponentFixture<EraseHistoryDialogComponent>;
+    let mockDialogRef: MatDialogRef<EraseHistoryDialogComponent>;
+
+    beforeEach(async () => {
+        mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+        await TestBed.configureTestingModule({
+            imports: [EraseHistoryDialogComponent],
+            providers: [{ provide: MatDialogRef, useValue: mockDialogRef }],
+        }).compileComponents();
+    });
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            declarations: [EraseHistoryDialogComponent],
+            imports: [EraseHistoryDialogComponent],
         });
         fixture = TestBed.createComponent(EraseHistoryDialogComponent);
         component = fixture.componentInstance;
@@ -17,5 +27,15 @@ describe('EraseHistoryDialogComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should close dialog without value on no click', () => {
+        component.onNoClick();
+        expect(mockDialogRef.close).toHaveBeenCalledWith();
+    });
+
+    it('should close dialog with true value on yes click', () => {
+        component.onYesClick();
+        expect(mockDialogRef.close).toHaveBeenCalledWith(true);
     });
 });
