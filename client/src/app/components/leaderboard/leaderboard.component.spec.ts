d72848d65187139d8f84cba0d87ff7d828f17955
marcantoinem <marc-antoine.m@outlook.com>
2024-04-11 14:11:37 -0400

    Improve admin leaderboard table style

--- client/src/app/components/leaderboard/leaderboard.component.spec.ts
+++ client/src/app/components/leaderboard/leaderboard.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { SortOption } from '@app/enums/sort-option';
 import { GameService } from '@app/services/game/game.service';
@@ -38,7 +39,15 @@ describe('LeaderboardComponent', () => {
         mockSortOption = SortOption.ScoreAscending;
 
         TestBed.configureTestingModule({
-            imports: [LeaderboardComponent, CommonModule, MatIconModule, MatButtonModule, BrowserAnimationsModule, NoopAnimationsModule],
+            imports: [
+                LeaderboardComponent,
+                CommonModule,
+                MatIconModule,
+                MatButtonModule,
+                BrowserAnimationsModule,
+                NoopAnimationsModule,
+                MatTableModule,
+            ],
             providers: [
                 { provide: MatSnackBar, useValue: snackBarMock },
                 { provide: GameService, useValue: gameService },
@@ -75,14 +84,36 @@ describe('LeaderboardComponent', () => {
         });
     });
 
+    describe('getTextState', () => {
+        it('should return correct class for NoInteraction', () => {
+            expect(component.getTextState(UserState.NoInteraction)).toEqual('Aucune interaction');
+        });
+
+        it('should return correct class for FirstInteraction', () => {
+            expect(component.getTextState(UserState.FirstInteraction)).toEqual('Réponse choisie');
+        });
+
+        it('should return correct class for AnswerConfirmed', () => {
+            expect(component.getTextState(UserState.AnswerConfirmed)).toEqual('Réponse confirmée');
+        });
+
+        it('should return correct class for FinalResults', () => {
+            expect(component.getTextState(UserState.FinalResults)).toEqual('Résultats finaux');
+        });
+
+        it('should return correct class for Disconnect', () => {
+            expect(component.getTextState(UserState.Disconnect)).toEqual('Déconnecté');
+        });
+    });
+
     describe('setOptionSort', () => {
         it('should set game service sort option correctly for user ascending', () => {
-            component.setOptionSort({ active: 'user', direction: 'asc' });
+            component.setOptionSort({ active: 'username', direction: 'asc' });
             expect(gameService.sortOption).toEqual(SortOption.UsernameAscending);
         });
 
         it('should set game service sort option correctly for user descending', () => {
-            component.setOptionSort({ active: 'user', direction: 'desc' });
+            component.setOptionSort({ active: 'username', direction: 'desc' });
             expect(gameService.sortOption).toEqual(SortOption.UsernameDescending);
         });
 
