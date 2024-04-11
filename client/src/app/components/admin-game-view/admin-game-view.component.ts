dcc9359be041d7336fdc218a67274532bb96b068
Charles-Olivier Caron <charlo3112@gmail.com>
2024-04-11 19:59:03 -0400

    GameState enum values updated in file

--- client/src/app/components/admin-game-view/admin-game-view.component.ts
+++ client/src/app/components/admin-game-view/admin-game-view.component.ts
@@ -19,29 +19,29 @@ export class AdminGameViewComponent {
     constructor(readonly gameService: GameService) {}
 
     get buttonText(): string {
-        return this.gameService.currentState === GameState.LastQuestion ? 'Résultats' : 'Prochaine Question';
+        return this.gameService.currentState === GameState.LAST_QUESTION ? 'Résultats' : 'Prochaine Question';
     }
 
     enableNextStepButton(): boolean {
-        return this.gameService.currentState === GameState.ShowResults || this.gameService.currentState === GameState.LastQuestion;
+        return this.gameService.currentState === GameState.SHOW_RESULTS || this.gameService.currentState === GameState.LAST_QUESTION;
     }
 
     canPause(): boolean {
-        return this.gameService.currentState === GameState.AskingQuestion;
+        return this.gameService.currentState === GameState.ASKING_QUESTION;
     }
 
     canPanic(): boolean {
         return (
             !this.gameService.panic &&
             this.gameService.currentQuestion !== undefined &&
-            this.gameService.currentState === GameState.AskingQuestion &&
+            this.gameService.currentState === GameState.ASKING_QUESTION &&
             ((this.gameService.currentQuestion.type === QuestionType.QCM && this.gameService.time > MIN_TIME_PANIC_QCM_S) ||
                 (this.gameService.currentQuestion.type === QuestionType.QRL && this.gameService.time > MIN_TIME_PANIC_QRL_S))
         );
     }
 
     nextStep(): void {
-        if (this.gameService.currentState === GameState.LastQuestion) {
+        if (this.gameService.currentState === GameState.LAST_QUESTION) {
             this.gameService.showFinalResults();
         } else {
             this.gameService.nextQuestion();
