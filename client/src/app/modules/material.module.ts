75651512551abbcceca5c889dff5db4235d381a0
Charles-Olivier Caron <charlo3112@gmail.com>
2024-04-11 14:35:19 -0400

    material module created

---
+++ client/src/app/modules/material.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const modules = [
+    // MatAutocompleteModule,
+    MatButtonModule,
+    // MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    // MatChipsModule,
+    // MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatDividerModule,
+    // MatMenuModule,
+    // MatPaginatorModule,
+    // MatProgressBarModule,
+    // MatProgressSpinnerModule,
+    MatRadioModule,
+    MatSelectModule,
+    // MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    // MatSortModule,
+    // MatStepperModule,
+    // MatTableModule,
+    // MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+];
+
+/**
+ * Material module
+ * IMPORTANT : IMPORT ONLY USED MODULES !!!!!!
+ */
+@NgModule({
+    imports: [...modules],
+    exports: [...modules],
+    providers: [],
+})
+export class AppMaterialModule {}
